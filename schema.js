c18ad28e5897bc4f17a6a03a984195bac4ab5d5d
Vignesh Mohan <vignesh.mohan@colanonline.com>
2024-07-09 19:17:56 +0530

    GraphQl basics

---
+++ schema.js
@@ -0,0 +1,56 @@
+export const typeDefs = `#graphql
+
+type Game{
+    id:ID!,
+    title:String!,
+    platform:[String!]!,
+    reviews:[Review!]
+}
+
+type Review{
+    id:ID!,
+    rating:Int!,
+    content:String!
+    game:Game!
+    author:Author!
+}
+
+type Author{
+    id:ID!,
+    name:String!,
+    verified:Boolean!,
+    reviews:[Review!]
+
+}
+
+#Query
+type Query{
+    reviews:[Review]
+    review(id:ID!):Review
+    games:[Game]
+    game(id:ID!):Game
+    authors:[Author]
+    author(id:ID!):Author
+}
+
+#Mutation
+type Mutation{
+    addGame(game:AddGameInput):Game
+    deleteGame(id:ID!):[Game]
+    updateGame(id:ID!,edits:EditGameInput):Game
+}
+
+#input
+input AddGameInput{
+    title:String!,
+    platform:[String!]!
+}
+input EditGameInput{
+    title:String,
+    platform:[String!]
+}
+
+`
+
+
+//int,float,string,boolean,ID
